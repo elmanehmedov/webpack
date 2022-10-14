0b7af9fac776a99885318a4bb4008a14e68e4862
Elman <elmanehmedov66@gmail.com>
2022-10-14 12:02:02 +0400

    webpack-config

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+ import './style.css'
+  // import MyLogo from './assets/resource/myLogo.jpg'
+
+const App = () => {
+  return (
+    <div>
+     <h2>App</h2>
+     {/* <MyLogo/> */}
+    </div>
+   
+  )
+}
+
+export default App
